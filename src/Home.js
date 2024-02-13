a5c675e6360e1e81ba8dcf844b293adc1482781f
HKhai <hoangkhai.nt@outlook.com>
2024-02-13 16:58:58 -0600

    Added dummy posts to Home

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "First post", content: "lorem ipsum", author: "Khai" },
+    { id: 2, title: "Second post", content: "lorem ipsum", author: "Darrow" },
+    { id: 3, title: "Third post", content: "lorem ipsum", author: "Sevro" },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {posts.map((post) => (
+        <div className="blog-preview" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>By {post.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
